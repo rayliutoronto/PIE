eeadc7609387e1b16688cf431e7daefe61812a66
Ray Liu <rliu3@toronto.ca>
2018-08-08 00:36:40 -0400

    estimator

--- StartServing.sh
+++ StartServing.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /PIE/PIE
-nohup tensorflow_model_server  --port=9000 --model_name=pie --model_base_path=/PIE/output/export/BestExport/ > ../output/serving.log &
-nohup gunicorn --workers=20 api:app -b localhost:19000 > ../output/web_server.log &
+tensorflow_model_server  --port=9000 --model_name=pie --model_base_path=/PIE/output/export/BestExport/ &> ../output/serving.log &
+gunicorn --workers=20 api:app -b localhost:19000 &> ../output/web_server.log &
